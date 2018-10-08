23b09596eb22337e32d5dd8942669477aac28c2d
Agnieszka Wojtasik <wojtasikagnieszka2626@gmail.com>
2018-10-08 20:09:36 +0200

    sumazmiennych

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let liczbaPierwsza = 6;
+let liczbaDruga = 12;
+
+
+function suma(liczbaPierwsza, liczbaDruga) {
+    return liczbaPierwsza + liczbaDruga;       
+}
+
+let result = suma(liczbaPierwsza, liczbaDruga);
+console.log(result);
